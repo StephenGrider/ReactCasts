86d0dc1edd94f5b153cd700ff07a18966a469d5f
Stephen Grider <ste.grider@gmail.com>
2015-03-28 17:58:43 -0700

    exploring state and events - part 3

--- dropdown/src/button.jsx
+++ dropdown/src/button.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function() {
-    return <button className={"btn " + this.props.className} type="button">
+    return <button onClick={this.props.whenClicked} className={"btn " + this.props.className} type="button">
       {this.props.title}
       <span className={this.props.subTitleClassName}>{this.props.subTitle}</span>
     </button>
--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -7,10 +7,18 @@ var Button = require('./button');
 // var List = require('./list');
 
 module.exports = React.createClass({
+  handleClick: function() {
+    alert('hello from dropdown');
+  },
   render: function() {
 
     return <div className="dropdown">
-      <Button className="btn-default" title={this.props.title} subTitleClassName="caret" />
+      <Button
+        whenClicked={this.handleClick}
+        className="btn-default"
+        title={this.props.title}
+        subTitleClassName="caret"
+        />
     </div>
   }
 });
