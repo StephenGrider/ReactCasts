0233b4a4ab7b58ab02ac8b3c5de67d5dbaafaa44
stephen grider <ste.grider@gmail.com>
2015-06-01 23:42:24 -0700

    Firebase - Section 4

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,9 +1,16 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var rootUrl = 'https://blistering-torch-4253.firebaseio.com/';
 
 var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
+
     return <h1>
       Hello, React!
     </h1>
