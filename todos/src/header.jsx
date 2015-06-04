952235f4dae2006ea10df1f5c6b3ab46f9156b9c
stephen grider <ste.grider@gmail.com>
2015-06-03 22:13:14 -0700

    Firebase - Section 6

--- todos/src/header.jsx
+++ todos/src/header.jsx
@@ -1,14 +1,32 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
   render: function() {
     return <div className="input-group">
-      <input type="text" className="form-control" />
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text"
+        className="form-control" />
       <span className="input-group-btn">
-        <button className="btn btn-default" type="button">
+        <button
+          onClick={this.handleClick}
+          className="btn btn-default"
+          type="button">
           Add
         </button>
       </span>
     </div>
+  },
+  handleClick: function() {
+    console.log(this.state.text);
+  },
+  handleInputChange: function(event) {
+    this.setState({text: event.target.value});
   }
 });
