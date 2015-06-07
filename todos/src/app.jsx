6ff8591681c0e6bd39da04ff42d626a0225d5f18
stephen grider <ste.grider@gmail.com>
2015-06-07 00:04:36 -0700

    Firebase - Section 15

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -14,9 +14,9 @@ var App = React.createClass({
     }
   },
   componentWillMount: function() {
-    fb = new Firebase(rootUrl + 'items/');
-    this.bindAsObject(fb, 'items');
-    fb.on('value', this.handleDataLoaded);
+    this.fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(this.fb, 'items');
+    this.fb.on('value', this.handleDataLoaded);
   },
   render: function() {
     return <div className="row panel panel-default">
@@ -28,9 +28,32 @@ var App = React.createClass({
         <hr />
         <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
           <List items={this.state.items} />
+          {this.deleteButton()}
         </div>
       </div>
     </div>
+  },
+  deleteButton: function() {
+    if(!this.state.loaded) {
+      return
+    } else {
+      return <div className="text-center clear-complete">
+        <hr />
+        <button
+          type="button"
+          onClick={this.onDeleteDoneClick}
+          className="btn btn-default">
+          Clear Complete
+        </button>
+      </div>
+    }
+  },
+  onDeleteDoneClick: function() {
+    for(var key in this.state.items) {
+      if(this.state.items[key].done === true) {
+        this.fb.child(key).remove();
+      }
+    }
   },
   handleDataLoaded: function(){
     this.setState({loaded: true});
--- todos/src/list.jsx
+++ todos/src/list.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
     </div>
   },
   renderList: function() {
-    if(this.props.items && Object.keys(this.props.items).length === 0) {
+    if(!this.props.items) {
       return <h4>
         Add a todo to get started.
       </h4>
