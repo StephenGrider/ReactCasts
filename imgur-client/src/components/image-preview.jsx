7c86a668e90540e08fefdfa97e250b4164068da9
stephen grider <ste.grider@gmail.com>
2015-07-05 15:54:53 -0700

    Flux - 26 - Adding an Image Stats Overlay

--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -14,6 +14,14 @@ module.exports = React.createClass({
       >
       {this.props.animated && this.state.hovering ? this.video() : this.image()}
       {this.props.animated && !this.state.hovering ? this.icon() : null }
+      {this.state.hovering ? this.inset() : null}
+    </div>
+  },
+  inset: function() {
+    return <div className="inset">
+      Views: {this.props.views}
+      <br />
+      Upvotes: {this.props.ups}
     </div>
   },
   image: function() {
