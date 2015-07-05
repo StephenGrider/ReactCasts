fcfe2fc8b702fe77ba821595685250bd84423995
stephen grider <ste.grider@gmail.com>
2015-07-05 14:09:33 -0700

    Flux - 17 - Matching Parameters

--- imgur-client/src/routes.jsx
+++ imgur-client/src/routes.jsx
@@ -5,11 +5,12 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+var Topic = require('./components/topic');
 
 module.exports = (
   <Router history={new HashHistory}>
     <Route path="/" component={Main}>
-
+      <Route path="topics/:id" component={Topic} />
     </Route>
   </Router>
 )
