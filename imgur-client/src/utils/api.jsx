0d47fe042140ec39b098fb2f7dd197c1d15482ad
stephen grider <ste.grider@gmail.com>
2015-07-05 12:28:19 -0700

    Flux - 8 - Implementing Fetch

---
+++ imgur-client/src/utils/api.jsx
@@ -0,0 +1,13 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = '430d6820d865788';
+
+module.exports = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID ' + apiKey
+      }
+    });
+  }
+};
