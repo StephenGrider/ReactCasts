2528ac5875793ff75bdc047a793c44a20b7965e6
stephen grider <ste.grider@gmail.com>
2015-07-05 16:09:01 -0700

    Flux - 28 - Fetching Single Records from a Store

--- imgur-client/src/components/image-detail.jsx
+++ imgur-client/src/components/image-detail.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var Reflux = require('reflux');
+var ImageStore = require('../stores/image-store');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
   render: function() {
     return <div>
       I am an image detail.
     </div>
+  },
+  onChange: function(event, image) {
+    this.setState({
+      image: image
+    });
   }
 });
--- imgur-client/src/stores/image-store.jsx
+++ imgur-client/src/stores/image-store.jsx
@@ -15,6 +15,16 @@ module.exports = Reflux.createStore({
         this.triggerChange();
       }.bind(this));
   },
+  find: function(id){
+    var image = _.findWhere(this.images, {id: id});
+
+    if(image) {
+      return image
+    } else {
+      this.getImage(id);
+      return null
+    }
+  },
   triggerChange: function() {
     this.trigger('change', this.images);
   }
