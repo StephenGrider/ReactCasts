f6c8c35e14a262ca7ec042aa9be48a35dc1a6322
stephen grider <ste.grider@gmail.com>
2015-07-05 13:04:36 -0700

    Flux - 11 - Fetching Data Naively

--- imgur-client/src/components/topic-list.jsx
+++ imgur-client/src/components/topic-list.jsx
@@ -1,9 +1,31 @@
 var React = require('react');
+var Api = require('../utils/api');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    Api.get('topics/defaults')
+      .then(function(data){
+        this.setState({
+          topics: data.data
+        })
+      }.bind(this));
+  },
   render: function() {
     return <div className="list-group">
       Topic List
+      {this.renderTopics()}
     </div>
+  },
+  renderTopics: function() {
+    return this.state.topics.map(function(topic){
+      return <li>
+        {topic}
+      </li>
+    });
   }
-})
+});
