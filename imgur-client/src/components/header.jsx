b9a82f71231ccfcbc86c5eaa55b9a3b09a23996c
stephen grider <ste.grider@gmail.com>
2015-07-05 14:36:35 -0700

    Flux - 19 - React Router Helpers

--- imgur-client/src/components/header.jsx
+++ imgur-client/src/components/header.jsx
@@ -32,7 +32,7 @@ module.exports = React.createClass({
   renderTopics: function() {
     return this.state.topics.slice(0, 4).map(function(topic){
       return <li key={topic.id}>
-        <Link to={"topics/" + topic.id}>
+        <Link activeClassName="active" to={"topics/" + topic.id}>
           {topic.name}
         </Link>
       </li>
