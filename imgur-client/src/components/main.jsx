42bcfc608cd21c73c247aac2ac4d322146de86b2
stephen grider <ste.grider@gmail.com>
2015-07-05 12:48:26 -0700

    Flux - 10 - Display a List of Topics

--- imgur-client/src/components/main.jsx
+++ imgur-client/src/components/main.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
 var Header = require('./header');
+var TopicList = require('./topic-list');
 
 module.exports = React.createClass({
   render: function() {
     return <div>
       <Header />
-      {this.props.children}
+      {this.content()}
     </div>
+  },
+  content: function() {
+    if(this.props.children) {
+      return this.props.children
+    } else {
+      return <TopicList />
+    }
   }
 });
