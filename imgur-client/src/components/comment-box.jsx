9bfde960125d37d3ff3be0beb229fb79ac809a88
stephen grider <ste.grider@gmail.com>
2015-07-05 17:02:07 -0700

    Flux - 33 - CSS Animations

--- imgur-client/src/components/comment-box.jsx
+++ imgur-client/src/components/comment-box.jsx
@@ -2,9 +2,17 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function(){
-    return <div>
-      I am a comment box.
-      {this.props.comments}
-    </div>
+    return <ul className="list-group">
+      {this.renderComments()}
+    </ul>
+  },
+  renderComments: function() {
+    return this.props.comments.slice(0, 20).map(function(comment){
+      return <li className="list-group-item comment-box" key={comment.id}>
+        <span className="badge">{comment.ups}</span>
+        <h5>{comment.author}</h5>
+        {comment.comment}
+      </li>
+    })
   }
 });
